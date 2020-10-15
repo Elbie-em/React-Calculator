caf10740d7b9449d4d1b2aa0bce6f7c3c0fe76b5
Elbie-em <emoonga1993@gmail.com>
2020-10-15 20:01:14 +0200

    Remove inexistent class name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const App = () => {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
