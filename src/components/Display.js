0d929afd676089a49a78179d919de2d85e40d9a1
Elbie-em <emoonga1993@gmail.com>
2020-10-16 00:05:24 +0200

    Add styling to display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/styles.css';
 
 const Display = ({ result }) => (
-    <div>
+    <div className="display">
       {result}
     </div>
 );
