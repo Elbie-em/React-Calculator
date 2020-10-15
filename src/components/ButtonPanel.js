6c194bf01e948c7beb9fb6e80d60a6f3b628b214
Elbie-em <emoonga1993@gmail.com>
2020-10-15 22:44:11 +0200

    Add ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import Button from './Button';
+
+const buttonSyms = [
+  ['AC', '+/-', '%', '÷'],
+  ['7', '8', '9', 'X'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '='],
+];
+
+const ButtonPanel = () => {
+  return (
+    <div>
+      {buttonSyms.map(group => (
+        <div key={group}>
+          {group.map(sym => <Button key={sym} name={sym}>{sym}</Button>)}
+        </div>
+      ))}
+    </div>
+  )
+};
+
+export default ButtonPanel;
