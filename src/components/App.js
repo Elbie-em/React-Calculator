40d383d2d542464edff2ad2ced3e77c614550fcb
Elbie-em <emoonga1993@gmail.com>
2020-10-15 22:45:09 +0200

    Implement App but calling Display and Button Panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import './styles.css';
+import '../styles.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 const App = () => (
     <div className="app">
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display />
+      <ButtonPanel />
     </div>
 );
 
