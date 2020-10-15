7b83ccb8ba7dd5a455ffeeb94a71aa2b076d387f
Elbie-em <emoonga1993@gmail.com>
2020-10-15 21:30:43 +0200

    Move App component to components directory

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './styles.css';
+
+const App = () => (
+    <div className="app">
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Big from 'big.js';
 // eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
 // eslint-disable-next-line no-unused-vars
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
