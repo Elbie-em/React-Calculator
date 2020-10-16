c7dbbc5f03389ad671cb398789320fe41887aa80
Elbie-em <emoonga1993@gmail.com>
2020-10-16 22:00:52 +0200

    Add switch statement to check which operand is being used for calculation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,7 +2,28 @@ import Big from 'big.js';
 
 const operate = (numberOne,numberTwo,operation) => {
   let data;
-  
+
+  switch (operation) {
+    case '÷':
+      data = numberTwo === '0' ? 0 : Big(numberOne).div(numberTwo).toString();
+      break;
+    case 'X':
+      data = Big(numberOne).times(numberTwo).toString();
+      break;
+    case '+':
+      data = Big(numberOne).plus(numberTwo).toString();
+      break;
+    case '-':
+      data = Big(numberOne).minus(numberTwo).toString();
+      break;
+    case '%':
+      data = Big(numberOne).div('100').times(numberTwo).toString();
+      break;
+    default:
+      break;
+  }
+
+  return data;
 
 }
 
