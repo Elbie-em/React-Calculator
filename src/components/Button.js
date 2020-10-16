000f60bb25dead383099c123c1553dad12488d0f
Elbie-em <emoonga1993@gmail.com>
2020-10-16 02:17:02 +0200

    Add more prop types and default  and edit class name

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,14 +3,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/styles.css';
 
-const Button = ({ name }) => (
-  <button className="btn" type="button">
+const Button = ({ name ,color, wide}) => (
+  <button type="button" 
+  className={`btn btn-${color} ${wide ? 'btn-wide':''}`} color={color}>
     {name}
   </button>
 );
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  wide: PropTypes.bool.isRequired,
+  color: PropTypes.string,
 };
+
+Button.defaultProps = {
+  color: 'orange',
+};
+
 
 export default Button;
